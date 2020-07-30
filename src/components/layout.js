203d7674b434efc22e8bf8d10bde021cc4b6b2f9
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-30 11:17:33 +0900

    remove unnecessary components

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,34 +7,18 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import "twin.macro"
 
-import Header from "./header"
-
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div tw="container px-4 pt-0 pb-6 mx-auto my-0 max-w-screen-lg">
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
     </>
   )
 }
