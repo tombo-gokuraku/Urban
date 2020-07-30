2790c0084f02b709da2ad377d87b67c780c6ceb5
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-30 11:47:51 +0900

    add P

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Hero from "../components/Hero"
 import H1 from "../components/H1"
 import H2 from "../components/H2"
 import H3 from "../components/H3"
+import P from "../components/P"
 
 const IndexPage = () => {
   return (
@@ -19,7 +20,23 @@ const IndexPage = () => {
       </Hero>
       <H2>Aliquam ipsum purus dolor</H2>
       <H3>Maecenas a gravida quam</H3>
-      <H3 white>Suspendisse quis massa vel justo</H3>
+      <H3 reverse>Suspendisse quis massa vel justo</H3>
+      <P>
+        Etiam posuere hendrerit arcu, ac blandit nulla. Sed congue malesuada
+        nibh, a varius odio vehicula aliquet. Aliquam consequat, nunc quis
+        sollicitudin aliquet, enim magna cursus auctor lacinia nunc ex blandit
+        augue. Ut vitae neque fermentum, luctus elit fermentum, porta augue.
+        Nullam ultricies, turpis at fermentum iaculis, nunc justo dictum dui,
+        non aliquet erat nibh non ex.
+      </P>
+      <P reverse>
+        Etiam posuere hendrerit arcu, ac blandit nulla. Sed congue malesuada
+        nibh, a varius odio vehicula aliquet. Aliquam consequat, nunc quis
+        sollicitudin aliquet, enim magna cursus auctor lacinia nunc ex blandit
+        augue. Ut vitae neque fermentum, luctus elit fermentum, porta augue.
+        Nullam ultricies, turpis at fermentum iaculis, nunc justo dictum dui,
+        non aliquet erat nibh non ex.
+      </P>
     </Layout>
   )
 }
