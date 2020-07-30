bc06838c7b25e0be94e1dbf61f55cad65a287fa8
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-30 11:34:21 +0900

    add font-bold

--- src/components/H2.js
+++ src/components/H2.js
@@ -1,5 +1,5 @@
 import tw from "twin.macro"
 
-const H2 = tw.h2`text-black xl:text-4xl md:text-3xl text-2xl mb-4`
+const H2 = tw.h2`text-black xl:text-4xl md:text-3xl text-2xl mb-4 font-bold`
 
 export default H2
