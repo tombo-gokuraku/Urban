43781e40a9fd36d65fd714c1509385915cbb0ecc
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-31 09:02:25 +0900

    adjust Button font-size

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import tw, { styled } from "twin.macro"
 
 const Button = styled.a(({ reverse }) => [
-  tw`inline-block px-4 py-2 text-base font-bold text-center text-white no-underline whitespace-no-wrap bg-blue-500 cursor-pointer rounded-md xl:text-2xl md:text-xl leading-10 hover:bg-blue-400 active:bg-blue-600 duration-200 transition-colors ease-in-out`,
+  tw`inline-block px-4 py-2 text-xl font-bold text-center text-white no-underline whitespace-no-wrap bg-blue-500 cursor-pointer rounded-md leading-10 hover:bg-blue-400 active:bg-blue-600 duration-200 transition-colors ease-in-out`,
   reverse && tw`text-blue-500 bg-white hover:bg-blue-100 active:bg-blue-200`,
 ])
 export default Button
