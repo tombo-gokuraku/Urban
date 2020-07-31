f0147d70feb63f27502d52d22a04d40fe50eec9e
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-31 10:06:21 +0900

    adjust margin

--- src/components/H1.js
+++ src/components/H1.js
@@ -1,5 +1,5 @@
 import tw from "twin.macro"
 
-const H1 = tw.h1`text-white xl:text-6xl md:text-5xl text-3xl mb-1 font-bold`
+const H1 = tw.h1`text-white xl:text-6xl md:( text-5xl mb-2 ) text-3xl mb-1 font-bold`
 
 export default H1
--- src/components/P.js
+++ src/components/P.js
@@ -1,7 +1,7 @@
 import tw, { styled } from "twin.macro"
 
 const P = styled.p(({ reverse }) => [
-  tw`text-base text-gray-600 xl:text-2xl md:text-xl`,
+  tw`mb-6 text-base text-gray-600 xl:text-2xl md:text-xl`,
   reverse && tw`text-blue-100`,
 ])
 export default P
