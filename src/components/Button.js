517e0370534c551123675b9122a9811f720587e1
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-04 11:26:52 +0900

    fix button string is not center bug

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,11 @@
-import tw, { styled } from "twin.macro"
+import tw, { styled, css } from "twin.macro"
 
 const Button = styled.a(({ reverse }) => [
-  tw`inline-block w-full px-4 py-2 text-xl font-bold text-center text-white no-underline whitespace-no-wrap align-middle bg-blue-700 cursor-pointer rounded-md hover:bg-blue-400 active:bg-blue-800 duration-200 transition-colors ease-in-out md:w-auto`,
+  tw`inline-block w-full px-4 text-xl font-bold text-center text-white no-underline whitespace-no-wrap align-middle bg-blue-700 cursor-pointer rounded-md hover:bg-blue-400 active:bg-blue-800 duration-200 transition-colors ease-in-out md:w-auto`,
   reverse && tw`text-blue-700 bg-white hover:bg-blue-100 active:bg-blue-200`,
+  css`
+    line-height: 2.95rem;
+    height: 2.7rem;
+  `,
 ])
 export default Button
